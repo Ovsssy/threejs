0adb6151180b47abfbab7a132c606820140f9659
Just_a_Fred <vasyanikolin5@gmail.com>
2025-03-15 19:07:24 +0500

    Transform controls and Orbit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import './style.css';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 
 const scene = new THREE.Scene();
 const clock = new THREE.Clock();
 let mixer;
+
 // Создаем DirectionalLight
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.0); // 1.0 - интенсивность
 scene.add(directionalLight);
@@ -35,19 +38,26 @@ renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 0.329; // Можно настроить уровень экспозиции
 renderer.direct = 1; 
 
-
-
 const loader = new GLTFLoader();
+let model;
+let animatedObject;
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
 
 loader.load( '../Blend/Objects/Punch.glb', function ( gltf ) {
-  const model = gltf.scene;
+  model = gltf.scene;
   scene.add(model);
 
+
+  
   if (gltf.animations.length > 0) {
     mixer = new THREE.AnimationMixer(model);
     const action = mixer.clipAction(gltf.animations[0]); // Берем первую анимацию
+    action.setLoop(THREE.LoopOnce);
+    action.clampWhenFinished = true;
     action.play();
-}
+  }
 
 }, undefined, function ( error ) {
 
@@ -55,14 +65,32 @@ loader.load( '../Blend/Objects/Punch.glb', function ( gltf ) {
 
 } );
 
+const transformControls = new TransformControls(camera, renderer.domElement);
+transformControls.attach(torus);
+
+console.log("transformControls.object:", transformControls.object);
+
+transformControls.addEventListener('dragging-changed', (event) => {
+  console.log("dragging-changed event fired!", event.value); 
+  orbitControls.enabled = !event.value;
+  console.log('OrbitControls:', orbitControls.enabled); // Проверяем в консоли
+});
+
+// Позволяем переключать режимы (перемещение, вращение, масштаб)
+window.addEventListener('keydown', (event) => {
+    if (event.key === 't') transformControls.setMode('translate'); // Перемещение
+    if (event.key === 'r') transformControls.setMode('rotate');    // Вращение
+    if (event.key === 's') transformControls.setMode('scale');     // Масштаб
+});
+
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
+  orbitControls.update();
 
   const delta = clock.getDelta(); // Получаем разницу времени между кадрами
   if (mixer) mixer.update(delta); // Обновляем анимацию
+
   renderer.render(scene, camera);
 }
 
